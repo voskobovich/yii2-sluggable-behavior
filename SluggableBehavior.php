80789a8eb021949c4aa91919f92ef008a1bcab3c
Vitaly Voskobovich <vitali.voskobovich@moonion.com>
2015-07-26 16:06:44 +0300

    First commit

---
+++ SluggableBehavior.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace voskobovich\behaviors;
+
+use Zelenin\yii\behaviors\Slug;
+
+
+/**
+ * Class SluggableBehavior
+ * @package voskobovich\behaviors
+ */
+class SluggableBehavior extends Slug
+{
+
+}
